04f26be7207dd48981a33d22ce7e7a1aadd3aed8
Gajesh Naik <robogajesh@gmail.com>
2023-04-09 19:02:14 +0400

    changed architecture and made it more smoother

--- contracts/Destination.sol
+++ contracts/Destination.sol
@@ -2,18 +2,14 @@
 pragma solidity ^0.8.9;
 
 import "./lzApp/NonblockingLzApp.sol";
-import "./interfaces/IStargateRouter.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Strings.sol";
-import "./interfaces/IStargateEthVault.sol";
 
 contract NFTMint is NonblockingLzApp, ERC721Enumerable {
     using SafeMath for uint256;
-    IStargateRouter public stargateRouter;
-    IStargateEthVault public stargateEthVault;
     
     string public PROVENANCE = ""; // IPFS URL WILL BE ADDED WHEN NFTS ARE ALL SOLD OUT
     
@@ -23,15 +19,27 @@ contract NFTMint is NonblockingLzApp, ERC721Enumerable {
 
     uint256 public constant price = 50000000000000000; // 0.05 ETH
 
-    uint public constant maxPurchase = 20;
+    uint public constant maxPurchase = 5;
 
     uint256 public constant MAX_NFTS = 100000;
 
     bool public saleIsActive = false;
     
     mapping(uint => string) public names;
+
+    struct SrcDetails {
+        bool allowed;
+        uint256 feeIfFailed;
+        uint256 feeInNative;
+    }
+
+    mapping(address => SrcDetails) public srcDetails;
+
+    IERC20 public weth;
+    // in case if the chain's native currency is ETH
+    bool public ethChain;
     
-    // Reserve 125 for team - Giveaways/Prizes etc
+    // Reserved NFTs
     uint public reserve = 100;
     
     event nameChange(address _by, uint _tokenId, string _name);
@@ -40,13 +48,11 @@ contract NFTMint is NonblockingLzApp, ERC721Enumerable {
 
     constructor(
         address _lzEndpoint,
-        address _stargateRouter,
         string memory _name,
         string memory _symbol,
-        address _stargateEthVault
+        bool _ethChain
     ) NonblockingLzApp(_lzEndpoint) ERC721(_name, _symbol) {
-        stargateRouter = IStargateRouter(_stargateRouter);
-        stargateEthVault = IStargateEthVault(_stargateEthVault);
+        ethChain = _ethChain;
     }
 
     function withdraw() public onlyOwner {
@@ -74,6 +80,17 @@ contract NFTMint is NonblockingLzApp, ERC721Enumerable {
 
     function flipSaleState() public onlyOwner {
         saleIsActive = !saleIsActive;
+    }
+
+    function setSrcDetails(
+        address _srcAddress,
+        bool _allowed,
+        uint256 _feeIfFailed,
+        uint256 _feeInNative
+    ) public onlyOwner {
+        srcDetails[_srcAddress].allowed = _allowed;
+        srcDetails[_srcAddress].feeIfFailed = _feeIfFailed;
+        srcDetails[_srcAddress].feeInNative = _feeInNative;
     }
     
     function tokensOfOwner(address _owner) external view returns(uint256[] memory ) {
@@ -110,11 +127,30 @@ contract NFTMint is NonblockingLzApp, ERC721Enumerable {
     }
     
     
-    function mint(uint numberOfTokens) public payable {
+    function mint(uint numberOfTokens) public {
         require(saleIsActive, "Sale must be active to mint NFT");
-        require(numberOfTokens > 0 && numberOfTokens <= maxPurchase, "Can only mint 20 tokens at a time");
-        require(totalSupply().add(numberOfTokens) <= MAX_NFTS, "Purchase would exceed max supply of Bananas");
-        require(msg.value >= price.mul(numberOfTokens), "Ether value sent is not correct");
+        require(numberOfTokens > 0 && numberOfTokens <= maxPurchase, "Can only mint 5 tokens at a time");
+        require(totalSupply().add(numberOfTokens) <= MAX_NFTS, "Purchase would exceed max supply of NFTs");
+
+        weth.transferFrom(
+            msg.sender,
+            address(this),
+            price.mul(numberOfTokens)
+        );
+        
+        for(uint i = 0; i < numberOfTokens; i++) {
+            uint mintIndex = totalSupply();
+            if (totalSupply() < MAX_NFTS) {
+                _safeMint(msg.sender, mintIndex);
+            }
+        }
+    }
+
+    function mintViaEth(uint numberOfTokens) public payable {
+        require(saleIsActive, "Sale must be active to mint NFT");
+        require(numberOfTokens > 0 && numberOfTokens <= maxPurchase, "Can only mint 5 tokens at a time");
+        require(totalSupply().add(numberOfTokens) <= MAX_NFTS, "Purchase would exceed max supply of NFTs");
+        require(numberOfTokens.mul(price) == msg.value, "not enough eth to mint");
         
         for(uint i = 0; i < numberOfTokens; i++) {
             uint mintIndex = totalSupply();
@@ -125,7 +161,7 @@ contract NFTMint is NonblockingLzApp, ERC721Enumerable {
     }
      
     function changeName(uint _tokenId, string memory _name) public {
-        require(ownerOf(_tokenId) == msg.sender, "Hey, your wallet doesn't own this banana!");
+        require(ownerOf(_tokenId) == msg.sender, "Hey, your wallet doesn't own this nft!");
         require(sha256(bytes(_name)) != sha256(bytes(names[_tokenId])), "New name is same as the current one");
         names[_tokenId] = _name;
         
@@ -144,36 +180,32 @@ contract NFTMint is NonblockingLzApp, ERC721Enumerable {
         uint64 _nonce, 
         bytes memory _payload
     ) internal override {
+        (address _user, uint256 _amountToMint) = abi.decode(_payload, (address, uint256));
 
-    }
+        address srcAddress;
+        assembly {
+            srcAddress := mload(add(_srcAddress, 20))
+        }
 
-    function sgReceive(
-        uint16 _chainId, 
-        bytes memory _srcAddress, 
-        uint _nonce, 
-        address _token, 
-        uint _amountLD, 
-        bytes memory _payload
-    ) external payable {
-        require(_token == address(0), "sgReceive: only native token supported");
-        require(msg.sender == address(stargateRouter), "Unauthorized");
-        require(_amountLD > 0, "_amount must be greater than 0");
+        require(srcDetails[srcAddress].allowed, "contract not allowed");
 
-        // Approve & Unwrap ETH
-        stargateEthVault.approve(address(stargateEthVault), _amountLD);
-        stargateEthVault.withdraw(_amountLD);
-
-        (address _depositor) = abi.decode(_payload, (address));
-
-        uint256 _amountToMint = _amountLD.div(price);
-
-        if (totalSupply().add(_amountToMint) <= MAX_NFTS) {
-            payable(_depositor).transfer(_amountLD);
+        
+        if (totalSupply().add(_amountToMint) <= MAX_NFTS || saleIsActive) {
+            bytes memory data = abi.encode(msg.sender, (_amountToMint * price) - srcDetails[srcAddress].feeIfFailed);
+        
+            _lzSend(
+                _srcChainId, 
+                data, 
+                payable(_user), 
+                address(0x0), 
+                bytes(""),
+                srcDetails[srcAddress].feeInNative
+            );
         } else {
-            for(uint i = 0; i < amount; i++) {
+            for(uint i = 0; i < _amountToMint; i++) {
                 uint mintIndex = totalSupply();
                 if (totalSupply() < MAX_NFTS) {
-                    _safeMint(_depositor, mintIndex);
+                    _safeMint(msg.sender, mintIndex);
                 }
             }
         }
--- contracts/Source.sol
+++ contracts/Source.sol
@@ -3,72 +3,80 @@ pragma solidity 0.8.19;
 
 import "./lzApp/NonblockingLzApp.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./interfaces/IStargateRouter.sol";
 import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "./interfaces/IStargateEthVault.sol";
+
+interface WETH {
+    function deposit() public payable;
+    function withdraw(uint256 wad) public;
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
 
 contract Source is NonblockingLzApp {
     using SafeMath for uint256;
 
     uint256 constant MAX_INT = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
 
-    IStargateRouter public stargateRouter;
     // Address of Destination contract where all the money will be sent
     address public destination;
     // Price for each NFT
     uint256 public constant price = 50000000000000000; // 0.05 ETH
     // Maximum amount of NFTs mintable in one transaction
-    uint public constant maxPurchase = 20;
+    uint public constant maxPurchase = 5;
     // Chain id of Destination Chain
-    uint16 dstChainId;
-    // Chain id of Source Chain
-    uint256 srcPoolId;
-    // Pool id of the Destination Chain
-    uint256 dstPoolId;
-    // Stargate ETH Vault
-    IStargateEthVault public stargateEthVault;
+    uint16 public dstChainId;
+    // WETH
+    WETH public weth;
 
     constructor(
         address _lzEndpoint,
-        address _stargateRouter,
         uint16 _dstChainId,
-        uint256 _srcPoolId,
-        uint256 _dstPoolId,
-        address _stargateEthVault
+        uint256 _weth
     ) NonblockingLzApp(_lzEndpoint) {
-        require(_stargateEthVault != address(0x0), "RouterETH: _stargateEthVault cant be 0x0");
-        require(_stargateRouter != address(0x0), "RouterETH: _stargateRouter cant be 0x0");
-        
-        stargateRouter = IStargateRouter(_stargateRouter);
-        stargateEthVault = IStargateEthVault(_stargateEthVault);
         dstChainId = _dstChainId;
-        srcPoolId = _srcPoolId;
-        dstPoolId = _dstPoolId;
+        weth = WETH(_weth);
     }
 
-    function omniMint(uint256 _amount, uint256 _fee) public payable {
-        require(_amount > 0, "need _amount > 0");
+    function omniMint(uint256 _amount) public payable {
+        require(_amount > 0 && _amount <= maxPurchase, "Can only mint 5 NFTs at a time");
         uint256 cost = _amount.mul(price);
-        require(msg.value > (cost + _fee), "stargate requires fee to pay crosschain message");
+        require(msg.value > 0, "stargate requires fee to pay crosschain message");
+
+        weth.transferFrom(msg.sender, address(this), cost);
+        weth.withdraw(weth.balanceOf(address(this)));
         
-        bytes memory data = abi.encode(msg.sender);
+        bytes memory data = abi.encode(msg.sender, _amount);
+        
+        _lzSend(
+            dstChainId, 
+            data, 
+            payable(_user), 
+            address(0x0), 
+            bytes(""),
+            msg.value
+        );
+    }
 
-        // wrap the ETH into WETH
-        IStargateEthVault(stargateEthVault).deposit{value: cost}();
-        IStargateEthVault(stargateEthVault).approve(address(stargateRouter), MAX_INT);
 
-        // Stargate's Router.swap() function sends the tokens to the destination chain.
-        stargateRouter.swap{value:_fee}(
-            dstChainId,                                     // the destination chain id
-            srcPoolId,                                      // the source Stargate poolId
-            dstPoolId,                                      // the destination Stargate poolId
-            payable(msg.sender),                            // refund adddress. if msg.sender pays too much gas, return extra eth
-            cost,                                           // total tokens to send to destination chain
-            cost,                                            // min amount allowed out
-            IStargateRouter.lzTxObj(200000, 0, "0x"),       // default lzTxObj
-            abi.encodePacked(destination),                   // destination address 
-            data                                            // bytes payload
+    function omniMintNative(uint256 _amount, uint256 _fee) public payable {
+        require(_amount > 0 && _amount <= maxPurchase, "Can only mint 5 NFTs at a time");
+        uint256 cost = _amount.mul(price);
+        require(msg.value > (cost + _fee), "eth is not enough");
+        
+        bytes memory data = abi.encode(msg.sender, _amount);
+        
+        _lzSend(
+            dstChainId, 
+            data, 
+            payable(_user), 
+            address(0x0), 
+            bytes(""),
+            _fee
         );
     }
 
@@ -78,6 +86,15 @@ contract Source is NonblockingLzApp {
         uint64 _nonce, 
         bytes memory _payload
     ) internal override {
+        address srcAddress;
+        assembly {
+            srcAddress := mload(add(_srcAddress, 20))
+        }
 
+        require(srcAddress == destination, "not sent by destination contract");
+
+        (address _user, uint256 _amountToSend) = abi.decode(_payload, (address, uint256));
+
+        payable(_user).transfer(_amountToSend);
     }
 }
